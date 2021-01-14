a663cc6e1b2a84f7dcb2caf329c15c57f6e42276
theafolayan <theafolayan@gmail.com>
2021-01-14 08:02:33 +0100

    ready to deploy

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<div class="footer_area">
+    <p>All rights reserved, Example developer &copy 2021</p>
+</div>
+<style>
+    .footer_area {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #4caf50;
+        color: white;
+        text-align: center;
+    }
+</style>
+
---
+++ src/routes/about.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Nav from "../components/Nav.svelte";
+	import Footer from "../components/Footer.svelte";
+</script>
+
+<svelte:head>
+	<title>About Page</title>
+</svelte:head>
+
+<Nav page="/about/"/>
+
+<main>
+    <h1>This is the About Page</h1>
+    <p> JungleJS is Easy to use!</p>
+</main>
+<Footer/>
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #859A00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
